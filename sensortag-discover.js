0e0ff1fcbdd007f0a0c00a179b73a32438dd6ffa
Oskari Okko Ojala <git@okko.fi>
2014-02-22 22:21:10 +0000

    Add nodejs to discover SensorTags

---
+++ sensortag-discover.js
@@ -0,0 +1,10 @@
+
+var SensorTag = require('sensortag');
+
+console.log('Please turn on the sensortag now.');
+console.log('Discovering... (CTRL+C to stop)');
+
+SensorTag.discover(function(sensorTag) {
+    console.log('Discovered sensorTag uuid' + sensorTag.uuid);
+});
+
